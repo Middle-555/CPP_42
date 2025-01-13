7f4e96f74f53f145413e887df329ee945482facf
Middle <kpourcel@student.42perpignan.fr>
2025-01-13 18:12:04 +0100

    Ajout fonction add

--- Module_00/ex_01/src/PhoneBook.cpp
+++ Module_00/ex_01/src/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 15:30:06 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/07 18:44:16 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:09:34 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,81 @@ PhoneBook::~PhoneBook()
 	std::cout<< "Repertory Closed" << std::endl;
 }
 
+
 void	PhoneBook::add()
 {
 	std::string	firstName, lastName, nickName,
 		phoneNumber, darkestSecret;
-	std::cout << "Enter First Name : " << std::endl;
-	std::getline(std::cin, firstName);
-	std::cout << "Enter Last Name : " << std::endl;
-	std::getline(std::cin, lastName);	
-	std::cout << "Enter Nickname : " << std::endl;
-	std::getline(std::cin, nickName);
-	std::cout << "Enter Phone number : " << std::endl;
-	std::getline(std::cin, phoneNumber);
-	std::cout << "Enter Darkest secret : " << std::endl;
-	std::getline(std::cin, darkestSecret);
-	
-	if (firstName != "" && lastName != "" && nickName != "" &&
-	phoneNumber != "" && darkestSecret != "")
-		this->repertory[this->index % 8].setContact(firstName, lastName, nickName,
-			phoneNumber, darkestSecret);
+	int			isValid;
+	int			i;
+
+	while (firstName.empty())
+	{
+		std::cout << "Enter First Name:" << std::endl;
+		std::getline(std::cin, firstName);
+		if (firstName.empty())
+			std::cout << "Error: This field cannot be empty." << std::endl;
+	}
+	while (lastName.empty())
+	{
+		std::cout << "Enter Last Name:" << std::endl;
+		std::getline(std::cin, lastName);
+		if (lastName.empty())
+			std::cout << "Error: This field cannot be empty." << std::endl;
+	}
+	while (nickName.empty())
+	{
+		std::cout << "Enter Nickname:" << std::endl;
+		std::getline(std::cin, nickName);
+		if (nickName.empty())
+			std::cout << "Error: This field cannot be empty." << std::endl;
+	}
+	while (true)
+	{
+		std::cout << "Enter Phone Number:" << std::endl;
+		std::getline(std::cin, phoneNumber);
+		if (phoneNumber.empty())
+		{
+			std::cout << "Error: This field cannot be empty." << std::endl;
+			continue;
+		}
+		isValid = 1;
+		i = 0;
+		while (i < phoneNumber.length())
+		{
+			if (!isdigit(phoneNumber[i]))
+			{
+				isValid = 0;
+				break;
+			}
+			i++;
+		}
+		if (!isValid)
+		{
+			std::cout << "Error: The phone number must contain only digits." << std::endl;
+			continue;
+		}
+		break;
+	}
+	while (darkestSecret.empty())
+	{
+		std::cout << "Enter Darkest Secret:" << std::endl;
+		std::getline(std::cin, darkestSecret);
+		if (darkestSecret.empty())
+			std::cout << "Error: This field cannot be empty." << std::endl;
+	}
+
+	this->repertory[this->index % 8].setFirstName(firstName);
+	this->repertory[this->index % 8].setLastName(lastName);
+	this->repertory[this->index % 8].setNickName(nickName);
+	this->repertory[this->index % 8].setPhoneNumber(phoneNumber);
+	this->repertory[this->index % 8].setDarkestSecret(darkestSecret);
 	this->index++;
-	std::cout << "All informations are successfuly registered !" 
-		<< std::endl;
+	std::cout << "All information has been successfully registered!" << std::endl;
 }
+
 
 void	PhoneBook::search()
 {
-	if (!this->repertory[0].getContact().size())
+	if (!this->repertory[0].getContact().empty())
 }
