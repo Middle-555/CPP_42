947bc96406eb99a64cb14cef575ff2d3f1ed5adf
Middle <pourcel.kevin@gmail.com>
2025-01-21 14:59:23 +0100

    Module01 exO2 brain fini + setup de l'ex03

--- Module_01/ex03/include/HumanB.hpp
+++ Module_01/ex03/include/HumanB.hpp
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   brain.hpp                                          :+:      :+:    :+:   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 15:20:56 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/21 14:29:27 by kpourcel         ###   ########.fr       */
+/*   Created: 2025/01/21 14:53:37 by kpourcel          #+#    #+#             */
+/*   Updated: 2025/01/21 14:53:38 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BRAIN_HPP
-# define BRAIN_HPP
-
-#include <iostream>
-
-class brain
-{
-private:
-
-public:
-	brain();
-	~brain();
-	std:: string str;
-};
-
-
-
-#endif 
--- Module_01/ex03/src/main.cpp
+++ Module_01/ex03/src/main.cpp
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   brain.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 15:20:56 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/21 14:29:27 by kpourcel         ###   ########.fr       */
+/*   Created: 2025/01/21 14:53:50 by kpourcel          #+#    #+#             */
+/*   Updated: 2025/01/21 14:53:51 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BRAIN_HPP
-# define BRAIN_HPP
-
-#include <iostream>
-
-class brain
-{
-private:
-
-public:
-	brain();
-	~brain();
-	std:: string str;
-};
-
-
-
-#endif 
