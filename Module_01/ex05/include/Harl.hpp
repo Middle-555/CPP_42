18da8a5cbcb611d75cfe089b480ff8e6fd374135
Middle <pourcel.kevin@gmail.com>
2025-01-23 21:18:29 +0100

    Init Exo 5 Module 1

--- Module_01/ex05/include/Harl.hpp
+++ Module_01/ex05/include/Harl.hpp
@@ -6,12 +6,12 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:55:43 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/23 18:05:10 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/01/23 18:49:04 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef 	Harl_HPP
-# define 	Harl_HPP
+#ifndef 	HARL_HPP
+# define 	HARL_HPP
 
 #include <iostream>
 #include <string>
