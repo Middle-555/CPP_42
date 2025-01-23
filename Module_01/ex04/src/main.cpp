87b29742190584e7f9509904905830d52d19ec20
Middle <pourcel.kevin@gmail.com>
2025-01-23 17:21:40 +0100

    Commentaire + Suppréssion des test nécessaires

--- Module_01/ex04/src/main.cpp
+++ Module_01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 22:01:55 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/22 01:38:55 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/01/23 17:18:44 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,43 @@
 
 int	main(int argc, char **argv)
 {
-	std::string	s1 = argv[2];
 	if (argc != 4)
 	{
 		std::cout << " Use ./replace <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
-		
-	else if (!s1.empty())
+	Sed		sed;
+	std::string	file = argv[1];
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
+	std::string	line;
+	std::string 	fileContent;
+	
+	// test
+	std::cout << "Fichier : " << file << std::endl;
+	std::cout << "s1 (à remplacer) : '" << s1 << "'" << std::endl;
+	std::cout << "s2 (remplacement) : '" << s2 << "'" << std::endl;
+
+	// Ouverture du fichier 
+	std::ifstream	inputFile(file);
+	if (!inputFile)
 	{
-		std::cout << "file empty" << std::endl;
+		std::cout << "Can't open the file." << std::endl;
+		return (1);
+	}
+	if (s1.empty())
+	{
+		std::cout << "there is nothing to replace" << std::endl;
 		return (1);
 	}
 	else
 	{
-		
+		while (std::getline(inputFile, line))
+		{
+			fileContent += line + "\n";
+		}
+		inputFile.close();
+		std::string changed = sed.findAndReplace(fileContent, s1, s2);
+		sed.writeToFile(file, changed);
 	}
-	
 }
