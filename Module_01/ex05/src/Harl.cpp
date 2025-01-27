2a5355d041e5449e05b7e0c20d7d40476e8380f4
Middle <kpourcel@student.42perpignan.fr>
2025-01-27 19:14:28 +0100

    Module 01 ex05 fini

--- Module_01/ex05/src/Harl.cpp
+++ Module_01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:55:29 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/23 21:17:48 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/01/27 19:14:04 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,39 @@ Harl::	~Harl()
 	std::cout << "Destructor called" << std::endl;
 }
 
-void	Harl:: debug( void )
+void	Harl::debug( void )
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
-void	Harl:: info( void )
+void	Harl::info( void )
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void	Harl:: warning( void )
+void	Harl::warning( void )
 {
 	std::cout <<  "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void	Harl:: error( void )
+void	Harl::error( void )
 {
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	void (Harl::*actions[])(void) = { &Harl::debug, &Harl::info, \
+		&Harl::warning, &Harl::error };
+		
+	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*actions[i])();
+			return;
+		}
+	}
+	std::cout << "Unknow level !" << std::endl;
 }
--- Module_01/ex05/src/main.cpp
+++ Module_01/ex05/src/main.cpp
@@ -6,13 +6,31 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:55:36 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/23 19:43:45 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/01/27 19:11:29 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(void)
+int main()
 {
-	Harl	harl;
+    Harl harl;
+
+    std::cout << "Test avec DEBUG:\n";
+    harl.complain("DEBUG");
+
+    std::cout << "Test avec INFO:\n";
+    harl.complain("INFO");
+
+    std::cout << "Test avec WARNING:\n";
+    harl.complain("WARNING");
+
+    std::cout << "Test avec ERROR:\n";
+    harl.complain("ERROR");
+
+    std::cout << "Test avec un niveau inconnu:\n";
+    harl.complain("UNKNOWN");
+
+    return 0;
 }
+
