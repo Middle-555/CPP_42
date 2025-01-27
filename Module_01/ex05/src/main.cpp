e8c6de90d181426d89c5882861329294c6449211
Middle <kpourcel@student.42perpignan.fr>
2025-01-27 19:15:45 +0100

    update norme

--- Module_01/ex05/src/main.cpp
+++ Module_01/ex05/src/main.cpp
@@ -6,31 +6,20 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:55:36 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/01/27 19:11:29 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/01/27 19:15:38 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main()
+int	main()
 {
-    Harl harl;
+	Harl	harl;
 
-    std::cout << "Test avec DEBUG:\n";
-    harl.complain("DEBUG");
-
-    std::cout << "Test avec INFO:\n";
-    harl.complain("INFO");
-
-    std::cout << "Test avec WARNING:\n";
-    harl.complain("WARNING");
-
-    std::cout << "Test avec ERROR:\n";
-    harl.complain("ERROR");
-
-    std::cout << "Test avec un niveau inconnu:\n";
-    harl.complain("UNKNOWN");
-
-    return 0;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+    	harl.complain("UNKNOWN");
+	return 0;
 }
-
