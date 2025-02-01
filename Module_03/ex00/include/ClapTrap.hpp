2a02eedf42b78d8f77238a72950e24f0e9a55942
Middle <kpourcel@student.42perpignan.fr>
2025-02-01 20:20:22 +0100

    Init Module 03 ex00

--- Module_03/ex00/include/ClapTrap.hpp
+++ Module_03/ex00/include/ClapTrap.hpp
@@ -6,20 +6,28 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 20:09:14 by kpourcel          #+#    #+#             */
-/*   Updated: 2025/02/01 20:10:38 by kpourcel         ###   ########.fr       */
+/*   Updated: 2025/02/01 20:15:10 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAP_TRAP_HPP
 # define CLAP_TRAP_HPP
 
+#include <iostream>
+#include <string>
 class ClapTrap
 {
 private:
-
+	std::string _name;
+	int	_hitPoint = 10;
+	int	_energyPoint = 10;
+	int	_attackDamage = 10;
 public:
 	ClapTrap();
 	~ClapTrap();
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
 };
 
 #endif
